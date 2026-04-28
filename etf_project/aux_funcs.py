78b30f64f78a9ca99da7ac3ec1490e64dac04dc7
Edgar Huamantla <edgar@huamantla.biz>
2026-04-28 16:43:29 -0400

    created tree mapping -- need visualiser

--- etf_project/aux_funcs.py
+++ etf_project/aux_funcs.py
@@ -20,25 +20,33 @@ output: Plotly Figure
 import logging
 from bs4 import BeautifulSoup, Tag
 
-LOGGER: Final[logging.Logger] = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
-def extract_node_data(node:Tag, head_length:int=10) -> dict:
+def extract_node_data(node:Tag, head_length:int=10, boxover_pattern:str=None) -> dict:
     """
     extract metadata from a single HTML tag.
     """
+    raw_text = node.get_text(strip=True)
+    head_text = raw_text[:head_length] if raw_text else ""
 
-    head_text = node.get_text(strip=True)[:head_length] if node.string else ""
-
-    return {
+    node_data_dict = {
         "tag": node.name,
         "id": node.get('id', ''),
         "classes": ".".join(node.get('class', [])),
-        "content_head": text_content,
-        "full_label": f"<{node.name=}> class:{node.get('class', '')}"
+        "href": node.get('href', ''),
+        "content_head": head_text,
+        "full_label": f"<{node.name}> class:{node.get('class', '')}"
     }
 
+    match_value = node.get(boxover_pattern) if boxover_pattern else None
 
-def build_tree_data(node: BeautifulSoup | Tag, depth:int=0, max_depth:int=3) -> dict:
+    if match_value:
+        node_data_dict["boxover_match"] = match_value
+
+    return node_data_dict
+
+
+def build_tree_data(node: BeautifulSoup | Tag, depth:int=0, max_depth:int=3, tag_pattern=None) -> dict:
     """
     build a nested dictionary of the DOM structure with metadata
     """
@@ -47,15 +55,17 @@ def build_tree_data(node: BeautifulSoup | Tag, depth:int=0, max_depth:int=3) -> 
 
     node_name = node.name if node.name else "[DOCUMENT_ROOT]"
     
-    node = {
+    node_dict = {
         "name": f"{node_name} | {node.get('id', '')}",
-        "data": extract_node_data(node) if node.name else {},
+        "data": extract_node_data(node, boxover_pattern=tag_pattern) if node.name else {},
         "children": []
         }
 
     for child in node.find_all(recursive=False):
-        child_map = map_dom_to_dict(child, depth + 1, max_depth)
-        if child_map:
-            node["children"].append(child_map)
+        match child:
+            case Tag() as c:
+                child_map = build_tree_data(c, depth + 1, max_depth, tag_pattern)
+                if child_map:
+                    node_dict["children"].append(child_map)
 
-    return node
+    return node_dict

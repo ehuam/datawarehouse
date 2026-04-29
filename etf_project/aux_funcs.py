533cbd66afa8560bbaa7b2c1fec3c8c7223762d7
Edgar Huamantla <edgar@huamantla.biz>
2026-04-28 20:37:12 -0400

    created visualizer and tweaked functions, no longer needing ignore list because we aplpy labeling instead of filtering

--- etf_project/aux_funcs.py
+++ etf_project/aux_funcs.py
@@ -19,10 +19,14 @@ output: Plotly Figure
 
 import logging
 from bs4 import BeautifulSoup, Tag
+import plotly.graph_objects as go
+
 
 LOGGER = logging.getLogger(__name__)
 
-def extract_node_data(node:Tag, head_length:int=10, boxover_pattern:str=None) -> dict:
+def extract_node_data(
+        node:Tag,
+        head_length:int=10) -> dict:
     """
     extract metadata from a single HTML tag.
     """
@@ -30,17 +34,15 @@ def extract_node_data(node:Tag, head_length:int=10, boxover_pattern:str=None) ->
 
     node_data_dict = {
         "tag": node.name,
-        "id": node.get('id', ''),
-        "classes": ".".join(node.get('class', [])),
-        "href": node.get('href'),
+        "id": node.get('id'),
+        "classes": ".".join(node.get('class', [])) if node.get('class') else None,
         "content_head": raw_text[:head_length] if raw_text else None,
-        "full_label": f"<{node.name}> class:{node.get('class', '')}",
     }
 
-    match_value = node.get(boxover_pattern) if boxover_pattern else None
-
-    if match_value:
-        node_data_dict["boxover_match"] = match_value
+    for attr, value in node.attrs.items():
+        if attr.startswith('data-boxover'):
+            clean_key = attr.replace("data-boxover-", "")
+            node_data_dict[clean_key] = value
 
     no_empty_kv_pairs = {k: v for k,v in node_data_dict.items() if v}
         
@@ -64,7 +66,7 @@ def build_tree_data(
     raw_keys = [node.name, node.get('id')] + node_classes
     search_keys = [key for key in raw_keys if key]
     
-    node_data = extract_node_data(node, boxover_pattern=tag_pattern) if node.name else {}
+    node_data = extract_node_data(node) if node.name else {}
 
     for key in search_keys:
         if inventory_hash and key in inventory_hash:
@@ -95,3 +97,39 @@ def build_tree_data(
             node_dict["children"].append(child_map)
 
     return node_dict
+
+def build_visual_map(node_data) -> go.Figure:
+
+
+    ids, labels, parents, hover_texts = [], [], [], []
+
+    def recurse(node, parent_id=""):
+        node_id = f"{node['name']}_{id(node)}"
+
+        ids.append(node_id)
+        labels.append(node['name'])
+        parents.append(parent_id)
+
+        factset = node.get('data', {})
+        hover_info = "<br>".join([f"<b>{k}</b>: {v}" for k,v in factset.items()])
+        hover_texts.append(hover_info)
+
+        for child in node.get('children', []):
+            recurse(child, node_id)
+
+    recurse(node_data)
+    fig = go.Figure(go.Treemap(
+        ids=ids,
+        labels=labels,
+        parents=parents,
+        hovertext=hover_texts,
+        hoverinfo="text",
+        marker=dict(colorscale='Blues', showscale=True)
+    ))
+    fig.update_layout(
+        width=1000,
+        height=800,
+        margin=dict(t=30, l=10, r=10, b=10),
+        autosize=True
+    )
+    return fig
